d06af202fa1014125a0b5fe524f9692ec8cf8142
Crowdbotics <engineering@crowdbotics.com>
2023-02-28 19:03:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quan-back-t-3",
+  url: "https://quan-back-t-3.botics.co",
+  api: "https://quan-back-t-3.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
